80b772798dae30f1a33dbf0ce1344bf1d1f02c76
Adrian Dembek <adrian.dembek@truecaller.com>
2022-04-19 21:29:15 +0200

    data cleaning pipeline worked

--- kedro/src/main/pipelines/data_cleaning/pipeline.py
+++ kedro/src/main/pipelines/data_cleaning/pipeline.py
@@ -4,7 +4,28 @@ generated using Kedro 0.18.0
 """
 
 from kedro.pipeline import Pipeline, node, pipeline
-
+from helpers.data_cleaning import clean_customers, clean_articles, clean_transactions
 
 def create_pipeline(**kwargs) -> Pipeline:
-    return pipeline([])
+    return pipeline(
+        [
+            node(
+                name="customers",
+                func=clean_customers,
+                inputs="customers_raw",
+                outputs="customers_cleaned",
+            ),
+            node(
+                name="articles",
+                func=clean_articles,
+                inputs="articles_raw", 
+                outputs="articles_cleaned",
+            ),
+            node(
+                name="transactions",
+                func=clean_transactions,
+                inputs="transactions_raw", 
+                outputs=["transactions_cleaned", "baskets_cleaned"],
+            )
+        ]
+    )
